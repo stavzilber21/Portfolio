b7df5c01a03ff8afe81c811b2ce740034343f89a
stav zilber <93096648+stavzilber21@users.noreply.github.com>
2024-05-22 12:58:06 +0300

    Delete projects/React graduation project/src/firebase/usersFunction.js

--- projects/React graduation project/src/firebase/usersFunction.js
+++
@@ -1,7 +0,0 @@
-import db from '../firebase';
-import { addDoc, collection, deleteDoc, doc } from "firebase/firestore"
-
-export async function registerUser(user){
-    addDoc(collection(db, "users"), user)
-    alert("The user added to firebase!")
-}
