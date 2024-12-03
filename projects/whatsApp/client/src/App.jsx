1b4f0b5054b128bf3b77b769d6c3a781392cc761
stav zilber <93096648+stavzilber21@users.noreply.github.com>
2024-12-03 18:51:52 +0200

    Add files via upload

--- projects/whatsApp/client/src/App.jsx
+++ projects/whatsApp/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/home';
+import './index.css';  
 
 
 function App() {
