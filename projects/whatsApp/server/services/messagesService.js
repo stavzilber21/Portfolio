935b3a6292be1cbd37d58247c36f87393ebd5925
stav zilber <93096648+stavzilber21@users.noreply.github.com>
2024-12-26 15:21:31 +0200

    Delete projects/whatsApp/server/services/messagesService.js

--- projects/whatsApp/server/services/messagesService.js
+++
@@ -1,9 +0,0 @@
-const messagesRep = require('../repositories/messagesRep');
-
-const getAllMessages = async() => {
-    const messages =await messagesRep.getAllMessages();
-
-    return messages;
-}
-
-module.exports = {getAllMessages };
